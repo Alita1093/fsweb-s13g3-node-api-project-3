ace137b4b93ab1f9848d0e3162f58c495b1d2555
Alita1093 <furkanozturk1635@gmail.com>
2023-02-24 14:15:29 +0300

    s13g3

--- api/server.js
+++ api/server.js
@@ -1,12 +1,15 @@
-const express = require('express');
-
+const { json } = require("express");
+const express = require("express");
+const { logger } = require("./middleware/middleware");
 const server = express();
-
+const router = require("./users/users-router");
 // ekspres'in varsayılan olarak istek gövdelerinde JSON'u ayrıştıramayacağını unutmayın
-
+server.use(express.json());
 // global ara yazılımlar ve kullanıcı routelarının buraya bağlanması gerekir
+server.use("/api/users", router);
+server.use(logger);
 
-server.get('/', (req, res) => {
+server.get("/", (req, res) => {
   res.send(`<h2>Biraz ara yazılım yazalım!</h2>`);
 });
 
